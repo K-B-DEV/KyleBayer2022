b96dbe67e7ed77e2d67375dc7f5af2b19154094e
KyleBayer18 <kylebayerbusiness@gmail.com>
2022-07-19 11:18:53 -0400

    Crud functionality added for goals

--- backend/server.js
+++ backend/server.js
@@ -1,7 +1,11 @@
 const express = require("express");
-const dotenv = require("dotenv").config;
+const colors = require("colors");
+const dotenv = require("dotenv").config();
 const { errorHandler } = require("./middleware/errorMiddleware");
 const port = process.env.PORT || 8000;
+const connectDB = require("./config/db");
+
+connectDB();
 
 const app = express();
 
